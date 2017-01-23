21317876849b425ae43b8b06615537d9a3d78d99
smellmee <t4pita00@students.oamk.fi>
2017-01-23 12:52:37 +0200

    score system

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express')
+var bodyParser = require('body-parser');
+var app = express();
+
+var scores = [   
+
+  ];
+
+app.use(bodyParser.json());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+// GET /scores
+app.get('/scores', function(req, res){
+    console.log("tuut");
+    res.json(scores);
+});
+
+// POST /scores
+app.post('/scores', function(req, res){
+    scores.push({
+        name: req.body.name,
+        score: req.body.score
+    });
+
+    res.sendStatus(200);
+});
+
+
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port'));
+})
